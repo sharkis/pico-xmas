9e87c50a617dac46bd823b9f269978bf922ac608
Albert Sharkis <sharkis@sharkis.org>
2021-11-29 09:39:10 -0700

    blink xmas colors

--- xmas.c
+++ xmas.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
 #include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
 #include "ws2812.pio.h"
+#include <stdio.h>
 
-int main(){
-	PIO pio = pio0;
-    int sm_id = 0;
-    uint offset = pio_add_program(pio, &ws2812_program);
+int main() {
+  PIO pio = pio0;
+  int sm_id = 0;
+  uint offset = pio_add_program(pio, &ws2812_program);
 
-    ws2812_program_init(pio, sm_id, offset, 1, 800000, true);
+  ws2812_program_init(pio, sm_id, offset, 1, 800000, false);
+  int ledoffset = 0;
+  while (true) {
+	  for(int i=0;i<10;i++){
+		  if(i%2==ledoffset){
+        pio_sm_put_blocking(pio0, sm_id, 0xff000000);
+		  }
+		  else{
+        pio_sm_put_blocking(pio0, sm_id, 0x00ff0000);
+		  }
+	  }
 
-	gpio_init(25);
-	gpio_set_dir(25,GPIO_OUT);
-	while(true){
-		gpio_put(25,1);
-		for(int i=0;i<10;i++){
-
-
-	pio_sm_put_blocking(pio0,sm_id,0xffffffff);
-		}
-		sleep_ms(250);
-		gpio_put(25,0);
-		sleep_ms(250);
-	}
-
+	  ledoffset++;
+	  if(ledoffset==2)ledoffset=0;
+    sleep_ms(500);
+  }
 }
